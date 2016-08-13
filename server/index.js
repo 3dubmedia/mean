5fd0c63ff26422b327105a7466a84f509ced22a7
3dubmedia <jim@3dubmedia.com>
2016-08-12 18:36:38 -0700

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs');
+
+var app = express();
+var mongoose = require('mongoose'); // mongoose for mongodb
+var morgan = require('morgan'); // log requests to the console (express4)
+var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+mongoose.connect('mongodb://localhost/db_name_here');
+
+var staticRoot = __dirname + '/';
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(staticRoot));
+
+app.use(function(req, res, next){
+
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        return next();
+    }
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+
+});
+
+app.listen(app.get('port'), function() {
+    console.log('app running on port', app.get('port'));
+});
